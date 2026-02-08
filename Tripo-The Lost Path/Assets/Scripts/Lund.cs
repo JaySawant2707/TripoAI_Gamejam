fe4d9cff4adc88fffb4c6b1646119724732db80a
Jay Sawant <164754866+JaySawant2707@users.noreply.github.com>
2026-02-08 15:58:14 +0530

    Added a jugadu thing

---
+++ Tripo-The Lost Path/Assets/Scripts/Lund.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+
+public class Lund : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI orgasm;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            gameObject.GetComponentInChildren<MeshRenderer>().enabled = false;
+            StartCoroutine(ShowMessageRoutine());
+        }
+    }
+
+    IEnumerator ShowMessageRoutine()
+    {
+        orgasm.gameObject.SetActive(true);
+        yield return new WaitForSeconds(3f);
+        orgasm.gameObject.SetActive(false);
+    }
+}
