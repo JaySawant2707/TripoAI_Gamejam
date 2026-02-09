7e395ad029d1a6fed6136b82937ae40206ebc5e3
Jay Sawant <164754866+JaySawant2707@users.noreply.github.com>
2026-02-09 22:47:33 +0530

    final

---
+++ Tripo-The Lost Path/Assets/Scenes/NextLevelTimer.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevelTimer : MonoBehaviour
+{
+    [SerializeField] float delay;
+    [SerializeField] int sceneIndex;
+    [SerializeField] GameObject video;
+    private void Start() {
+        StartCoroutine(OpenNextScene());
+    }
+
+    IEnumerator OpenNextScene()
+    {
+        yield return new WaitForSeconds(12f);
+        video.SetActive(false);
+        yield return new WaitForSeconds(delay);
+        SceneManager.LoadScene(sceneIndex);
+    }
+}
